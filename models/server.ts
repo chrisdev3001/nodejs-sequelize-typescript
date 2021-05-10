f3b396f75b7cd6b6825a533f160f79a0824d505c
Chris Villarroel <cvillarroel@itaum.com>
2021-05-10 17:34:02 -0400

    Added controller, routes and updated server.ts

--- models/server.ts
+++ models/server.ts
@@ -1,12 +1,23 @@
 import express, { Application } from 'express'
+import userRoutes from '../routes/usuario'
 
 export default class Server{
     private app: Application
     private port: string
+    private paths = {
+        usuarios: '/api/usuarios'
+    }
 
     constructor(){
         this.app = express()
         this.port = process.env.PORT || '8000'
+
+        // Define routes
+        this.routes()
+    }
+
+    routes(){
+        this.app.use( this.paths.usuarios, userRoutes)
     }
 
     listen(){
