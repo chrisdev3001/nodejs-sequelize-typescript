8f8e3f6e767f861e5b227106c0b2ec8e5d2f0550
Chris Villarroel <cvillarroel@itaum.com>
2021-05-11 16:16:01 -0400

    Added Usuario model and get users from db

--- controllers/usuarios.ts
+++ controllers/usuarios.ts
@@ -1,19 +1,21 @@
 import { Request, Response } from "express"
+import Usuario from "../models/usuario"
 
 
-export const getUsuarios = (req: Request, res: Response) => {
-    res.json({
-        msg: 'getUsuarios'
-    })
+export const getUsuarios = async(req: Request, res: Response) => {
+    let usuarios = await Usuario.findAll()
+    
+    res.json({usuarios})
 }
 
-export const getUsuario = (req: Request, res: Response) => {
+export const getUsuario = async(req: Request, res: Response) => {
     const { id } = req.params
 
-    res.json({
-        msg: 'getUsuario',
-        id
-    })
+    let usuario = await Usuario.findByPk(id)
+    
+    !usuario && res.json(`No existe usuario con id: ${id}`)
+
+    res.json(usuario)
 }
 
 export const postUsuario = (req: Request, res: Response) => {
